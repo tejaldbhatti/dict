a71d8a4cc54ea95526e8ca4ec3e0fe655419221b
tejaldbhatti <82153829+tejaldbhatti@users.noreply.github.com>
2021-09-06 18:12:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template , request 
+from textblob import TextBlob
+from textblob import Word
+import nltk
+#download model
+nltk.download('punkt')
+from textblob import Word
+
+
+nltk.download('averaged_perceptron_tagger')
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('form.html')
+
+
+@app.route('/submit', methods = ['POST'])
+def form_data():
+   user_data = request.form.get('user_data')
+   blob = TextBlob(user_data)
+   output = Word(blob).definitions
+
+   return render_template('predict.html', data = f'{output}')
+
+if __name__ == '__main__':   
+        app.run(app.run(host='0.0.0.0', port=8080))   
